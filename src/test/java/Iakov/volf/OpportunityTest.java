15d08fbed624399d5a8ac4b52498c2f02702e6aa
ogoloub <repositary81>
2015-01-27 21:39:37 +0200

    Changes (clear errors) in class OpportunityTest

--- src/test/java/Iakov/volf/OpportunityTest.java
+++ src/test/java/Iakov/volf/OpportunityTest.java
@@ -8,12 +8,31 @@ package Iakov.volf;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.fail;
 
 public class OpportunityTest extends TestNgTestBase {
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
 
+    @Test
+    public void testOpportunitiesRemOnline() throws Exception {
+        driver.get(baseUrl + "/");
+        driver.findElement(By.xpath("//a[contains(text(),'Возможности')]")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if (isElementPresent(By.xpath("//*[@class='b-feature__title h-ta-c']"))) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
 
+        try {
+            assertEquals("Возможности", driver.findElement(By.xpath("//*[@class='b-feature__title h-ta-c']")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
 
     private boolean isElementPresent(By by) {
         try {
