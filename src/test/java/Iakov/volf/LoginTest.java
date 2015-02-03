5dd97bf8441e53f2f35a52c0aa0e352b86dd1fdd
Iakov Volf <jakoff@gmail.com>
2015-02-03 21:37:56 +0200

    Login button locator changed

--- src/test/java/Iakov/volf/LoginTest.java
+++ src/test/java/Iakov/volf/LoginTest.java
@@ -19,7 +19,7 @@ public class LoginTest extends TestNgTestBase {
     @Test
     public void testLogin() throws Exception {
         driver.get(baseUrl);
-        driver.findElement(By.xpath("//*[@class='b-navbar__entrance h-ml-15']")).click();
+        driver.findElement(By.xpath("//*[@class='js-auth-signin b-navbar__exit h-ml-10']")).click();
         driver.findElement(By.id("l-auth-login")).sendKeys("Mary");
         driver.findElement(By.id("l-auth-pass")).sendKeys("123456");
         driver.findElement(By.xpath("//div[3]/button")).click();
